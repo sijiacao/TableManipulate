c605e102c958e62a76f2c463392c9e974bd78eaf
Sijia Cao <sijiacao@Sijias-MacBook-Air.local>
2017-06-18 22:01:24 -0700

    add sort function to food and calory

--- src/script.js
+++ src/script.js
@@ -1,15 +1,15 @@
-function sortTable() {
+function sortFood() {
   var table, rows, switching, i, x, y, shouldSwitch;
   table = document.getElementById("myTable");
   switching = true;
 
   while (switching) {
-    switch = false;
+    switching = false;
     rows = table.getElementsByTagName("tr");
 
     for (i = 1; i < (rows.length - 1); i++){
       shouldSwitch = false;
-
+      debugger;
       x = rows[i].getElementsByTagName("td")[0];
       y = rows[i + 1].getElementsByTagName("td")[0];
 
@@ -25,3 +25,31 @@ function sortTable() {
       }
     }
   }
+
+  function sortCalory() {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("myTable");
+    switching = true;
+
+    while (switching) {
+      switching = false;
+      rows = table.getElementsByTagName("tr");
+
+      for (i = 1; i < (rows.length - 1); i++){
+        shouldSwitch = false;
+        debugger;
+        x = rows[i].getElementsByTagName("td")[1];
+        y = rows[i + 1].getElementsByTagName("td")[1];
+
+        if (x.innerHTML - y.innerHTML > 0) {
+        shouldSwitch= true;
+        break;
+        }
+      }
+
+      if (shouldSwitch) {
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+          switching = true;
+        }
+      }
+    }
